9f094aad06ed5c633c114af41df703234273e54e
choudharydivik <divik1820@outlook.com>
2025-03-16 11:18:02 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This will show top 3 food recommendations on swiggy home page.',
+    author='Divik Choudhary',
+    license='',
+)
